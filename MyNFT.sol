4b53849c8f8a0e1a8a6b70b7af364d0d04157640
Sandeep <40296898+sandy13869@users.noreply.github.com>
2023-08-25 15:34:53 +0530

    Initial Commit
    Tokenbound ERC6551 commit

---
+++ MyNFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    constructor() ERC721("myNFT", "MNF") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
